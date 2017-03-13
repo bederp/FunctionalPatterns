95db88c3e03ed19aa5ac672ec180f0b285d9aad5
kinder112 <voyager112@gmail.com>
2017-03-13 22:10:42 +0100

    IteratorExample

---
+++ src/main/java/iterator/functional/IteratorExample.java
@@ -0,0 +1,17 @@
+package iterator.functional;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IteratorExample {
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        integers.stream().forEach(i -> System.out.println(i));
+
+        integers.stream().forEach(System.out::println);
+
+        //careful this is different semantically
+        integers.forEach(System.out::println);
+    }
+}
