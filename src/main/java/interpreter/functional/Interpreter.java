bb907ab6ccb2c9c65f50ce5a7e3fd16bfb8e55bc
kinder112 <voyager112@gmail.com>
2017-03-14 22:12:54 +0100

    Added Interpreter pattern

---
+++ src/main/java/interpreter/functional/Interpreter.java
@@ -0,0 +1,36 @@
+package interpreter.functional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+import java.util.function.IntBinaryOperator;
+
+interface MathExpression {
+    Map<String, IntBinaryOperator> operationsMap = new HashMap<String, IntBinaryOperator>(){{
+        put("+", (int a, int b) -> a+b);
+        put("-", (int a, int b) -> a-b);
+    }};
+}
+
+class Parser {
+    private Stack<Integer> stack = new Stack<>();
+
+    int evaluate(String s) {
+        for (String token : s.split(" ")) {
+            IntBinaryOperator operation = MathExpression.operationsMap.get(token);
+            if (operation != null) {
+                stack.add(operation.applyAsInt(stack.pop(), stack.pop()));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
+
+public class Interpreter {
+    public static void main(String[] args) {
+        String s = "1 2 3 4 - + -";
+        System.out.println(new Parser().evaluate(s));
+    }
+}
---
+++ src/main/java/interpreter/gof/Interpreter.java
@@ -0,0 +1,79 @@
+package interpreter.gof;
+
+import java.util.Stack;
+
+interface MathExpression {
+    int evaluate();
+}
+
+class Addition implements MathExpression {
+
+    private final int value;
+
+    Addition(MathExpression left, MathExpression right) {
+        value = left.evaluate() + right.evaluate();
+    }
+
+    @Override
+    public int evaluate() {
+        return value;
+    }
+}
+
+class Subtraction implements MathExpression {
+    private final int value;
+
+    Subtraction(MathExpression left, MathExpression right) {
+        value = left.evaluate() - right.evaluate();
+    }
+
+    @Override
+    public int evaluate() {
+        return value;
+    }
+}
+
+class Value implements MathExpression {
+    private final int value;
+
+    Value(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public int evaluate() {
+        return value;
+    }
+}
+
+class Parser {
+    private Stack<MathExpression> stack = new Stack<>();
+
+    int evaluate(String s) {
+        for (String token : s.split(" ")) {
+            operatorLookup(token);
+        }
+        return stack.pop().evaluate();
+    }
+
+    private void operatorLookup(String token) {
+        switch (token) {
+            case "+":
+                stack.add(new Addition(stack.pop(), stack.pop()));
+                break;
+            case "-":
+                stack.add(new Subtraction(stack.pop(), stack.pop()));
+                break;
+            default:
+                stack.add(new Value(Integer.parseInt(token)));
+                break;
+        }
+    }
+}
+
+public class Interpreter {
+    public static void main(String[] args) {
+        String s = "1 2 3 4 - + -";
+        System.out.println(new Parser().evaluate(s));
+    }
+}
